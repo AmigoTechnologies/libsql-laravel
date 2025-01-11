3141ceb2d2c83a8707d66ec46ac2c57e1a887eb9
amigoodev <156919168+amigoodev@users.noreply.github.com>
2025-01-11 23:55:55 +0200

    Update LibsqlServiceProvider.php
    Fixing the way we accessing app->db , because in some cases resolving is not the way.

--- src/LibsqlServiceProvider.php
+++ src/LibsqlServiceProvider.php
@@ -13,82 +13,53 @@ use Illuminate\Database\Query\Builder;
 
 class LibsqlServiceProvider extends PackageServiceProvider
 {
-    public function boot(): void
-    {
-        parent::boot();
-        if (config('database.default') !== 'libsql') {
-            return;
-        }
+	public function boot(): void
+	{
+		parent::boot();
+		// Register the libsql driver
+		$this->app->make('db')->extend('libsql', function ($config, $name) {
+			$config['name'] = $name;
+			$config['driver'] = 'libsql';
 
-        Blueprint::macro('vectorIndex', function ($column, $indexName) {
-            /** @var Blueprint $this **/
-            return DB::statement("CREATE INDEX {$indexName} ON {$this->table}(libsql_vector_idx({$column}))");
-        });
+			return new LibsqlConnection(
+				function () use ($config) {
+					return new \Libsql\PDO(
+						$config["database"] ?? '',
+						password: $config["password"] ?? '',
+						options: $config
+					);
+				},
+				database: $config["database"],
+				config: $config,
+			);
+		});
 
-        Builder::macro('nearest', function ($indexName, $vector, $limit = 10) {
-            /** @var Builder $this **/
-            return $this->joinSub(
-                DB::table(DB::raw("vector_top_k('$indexName', '[" . implode(',', $vector) . "]', $limit)")),
-                'v',
-                "{$this->from}.rowid",
-                '=',
-                'v.id'
-            );
-        });
-    }
+		Blueprint::macro('vectorIndex', function ($column, $indexName) {
+			/** @var Blueprint $this **/
+			return DB::statement("CREATE INDEX {$indexName} ON {$this->table}(libsql_vector_idx({$column}))");
+		});
+
+		Builder::macro('nearest', function ($indexName, $vector, $limit = 10) {
+			/** @var Builder $this **/
+			return $this->joinSub(
+				DB::table(DB::raw("vector_top_k('$indexName', '[" . implode(',', $vector) . "]', $limit)")),
+				'v',
+				"{$this->from}.rowid",
+				'=',
+				'v.id'
+			);
+		});
+	}
 
 
-    public function configurePackage(Package $package): void
-    {
-        $package->name('libsql-laravel');
-    }
+	public function configurePackage(Package $package): void
+	{
+		$package->name('libsql-laravel');
+	}
 
-    public function register(): void
-    {
-        parent::register();
-        $this->app->singleton('db.factory', function ($app) {
-            return new class($app) extends ConnectionFactory {
-                protected function createConnection($driver, $connection, $database, $prefix = '', array $config = [])
-                {
-                    $connection = function () use ($config) {
-                        return new \Libsql\PDO(
-                            $config["database"] ?? '',
-                            password: $config["password"] ?? '',
-                            options: $config,
-                        );
-                    };
-
-                    var_dump($config);
-
-                    return new LibsqlConnection(
-                        $connection,
-                        database: $config["database"] ?? '',
-                        config: $config,
-                    );
-                }
-            };
-        });
-
-        $this->app->resolving('db', function (DatabaseManager $db) {
-            $db->extend('libsql', function ($config, $name) {
-                $config = config('database.connections.libsql');
-                $config['name'] = $name;
-                if (!isset($config['driver'])) {
-                    $config['driver'] = 'libsql';
-                }
-
-                return new LibsqlConnection(
-                    function () use ($config) {
-                        return new \Libsql\PDO(
-                            $config["database"] ?? '',
-                            password: $config["password"] ?? '',
-                            options: $config
-                        );
-                    },
-                    database: $config["database"],
-                    config: $config,
-                );
-            });
-        });
-    }
+	public function register(): void
+	{
+		parent::register();
+	}
 }
+
